e44d82f0ccc1640bd652d68415df86fbff8f658a
Guilherme <guilhermeciadope@gmail.com>
2022-09-20 21:05:24 -0300

    Update details.dart

--- lib/screen/home/details/details.dart
+++ lib/screen/home/details/details.dart
@@ -25,22 +25,96 @@ class Details extends StatelessWidget {
               contact.photo,
             ),
             Padding(
-              padding: EdgeInsets.all(16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(contact.name,
-                  style: Theme.of(context).textTheme.headline4,),
-                  Text(contact.phone,
-                  style: Theme.of(context).textTheme.subtitle1,),
-
-              
-                ],
-              ), 
+              padding: const EdgeInsets.all(
+                16,
+              ),
+              child: SizedBox(
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      contact.name,
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                    const Divider(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        action(
+                          icon: Icons.call,
+                          text: "Ligar",
+                        ),
+                        action(
+                          icon: Icons.message_outlined,
+                          text: "Mensagem SMS",
+                        ),
+                        action(
+                          icon: Icons.videocam,
+                          text: "Video",
+                        ),
+                        action(
+                          icon: Icons.mail_outline,
+                          text: "Enviar email",
+                        ),
+                      ],
+                    ),
+                    const Divider(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            const Icon(Icons.call),
+                            const SizedBox(
+                              width: 16,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(contact.phone),
+                                const Text("Celular"),
+                              ],
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: const [
+                            Icon(Icons.videocam),
+                            Icon(Icons.message_outlined)
+                          ],
+                        ),
+                      ],
+                    ),
+                    const Divider(),
+                    Row(
+                      children: [
+                        const Icon(Icons.email_outlined),
+                        const SizedBox(
+                          width: 8,
+                        ),
+                        Text(contact.phone),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             )
           ],
         ),
       ),
+    );
+  }
+
+  Column action({
+    required IconData icon,
+    required String text,
+  }) {
+    return Column(
+      children: [
+        Icon(icon),
+        Text(text),
+      ],
     );
   }
 }
